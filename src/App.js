3d2cad6a1f5d6d596399650408daa77b77e72d5d
AyushPratapSingh288 <100507774+AyushPratapSingh288@users.noreply.github.com>
2023-04-15 01:45:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Weather from './component/basics/weather app/weather'
+import Todo from './component/basics/Todo'
+
 
 const App = () => {
   return (
     <>
-     <Weather />
+     <Todo/>
     </>
   )
  
